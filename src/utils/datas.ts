f21be815e9688aa3e3ec5ca86893141a612e5bf6
shooh25 <koan6210@outlook.jp>
2023-05-21 16:54:43 +0900

    Update: computerモデルのカラム追加に対応

--- src/utils/datas.ts
+++ src/utils/datas.ts
@@ -1,4 +1,11 @@
 export const computerLabels = {
   cpu_name: "CPU",
   gpu_name: "GPU",
+  mb_name: "マザーボード",
+  memory_name: "メモリ",
+  ssd_name: "SSD",
+  hdd_name: "HDD",
+  case_name: "ケース",
+  cooler_name: "クーラー",
+  power_name: "電源",
 }
