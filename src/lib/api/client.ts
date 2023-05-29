57c8724356c0ddaa369ac355fea01b2b7a5a10e9
shooh25 <koan6210@outlook.jp>
2023-05-29 14:09:51 +0900

    Update: バックエンドのデプロイ先に接続

--- src/lib/api/client.ts
+++ src/lib/api/client.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const client = axios.create({
-  baseURL: "http://localhost:3001"
+  baseURL: "https://mygadgets.herokuapp.com/"
+  // baseURL: "http://localhost:3001"
 })
 
 export default client
